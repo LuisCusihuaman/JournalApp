84f5f01810db61bfd84393fe006839403fc998ca
Luis Cusihuaman <luiscusihuaman88@gmail.com>
2020-07-22 15:49:56 -0300

    chore: added node-sass

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { JournalApp } from "./JournalApp";
+import "./styles/styles.scss";
 ReactDOM.render(<JournalApp />, document.getElementById("root"));
