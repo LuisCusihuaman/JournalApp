c44a7df9150cd86091fd85bac2897ab095330f2a
Luis Cusihuaman <luiscusihuaman88@gmail.com>
2020-07-23 02:10:34 -0300

    feat: authrouter styles

--- src/routers/AuthRouter.js
+++ src/routers/AuthRouter.js
@@ -4,10 +4,18 @@ import { LoginScreen } from "../components/auth/LoginScreen";
 import { RegisterScreen } from "../components/auth/RegisterScreen";
 export const AuthRouter = () => {
 	return (
-		<Switch>
-			<Route exact path="/auth/login" component={LoginScreen} />
-			<Route exact path="/auth/register" component={RegisterScreen} />
-			<Redirect to="/auth/login" />
-		</Switch>
+		<div className="auth__main">
+			<div className="auth__box-container">
+				<Switch>
+					<Route exact path="/auth/login" component={LoginScreen} />
+					<Route
+						exact
+						path="/auth/register"
+						component={RegisterScreen}
+					/>
+					<Redirect to="/auth/login" />
+				</Switch>
+			</div>
+		</div>
 	);
 };
