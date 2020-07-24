77ddd93b400dd0d0da3db23612b4c911472f7d97
Luis Cusihuaman <luiscusihuaman88@gmail.com>
2020-07-24 17:38:40 -0300

    chore(app): configured react redux

--- src/JournalApp.js
+++ src/JournalApp.js
@@ -1,6 +1,12 @@
 import React from "react";
+import { Provider } from "react-redux";
+import { store } from "./store/store";
 import { AppRouter } from "./routers/AppRouter";
 
 export const JournalApp = () => {
-	return <AppRouter />;
+	return (
+		<Provider store={store}>
+			<AppRouter />;
+		</Provider>
+	);
 };
---
+++ src/store/store.js
@@ -0,0 +1,5 @@
+import { createStore, combineReducers } from "redux";
+import { authReducer } from "../reducers/authReducer";
+
+const reducers = combineReducers({ auth: authReducer });
+export const store = createStore(reducers);
