a02965b867bb58894eb89c9e711816a901685241
Luis Cusihuaman <luiscusihuaman88@gmail.com>
2020-07-25 04:20:32 -0300

    feat(approuter): show a global loading

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import {
 	BrowserRouter as Router,
 	Route,
@@ -13,13 +13,25 @@ import { login } from "../actions/auth";
 
 export const AppRouter = () => {
 	const dispatch = useDispatch();
+	const [checking, setChecking] = useState(true);
+	const [isLoggedIn, setIsLoggedIn] = useState(false);
+
 	useEffect(() => {
 		firebase.auth().onAuthStateChanged((user) => {
 			if (user?.uid) {
 				dispatch(login(user.uid, user.displayName));
+				setIsLoggedIn(true);
+			} else {
+				setIsLoggedIn(false);
 			}
+			setChecking(false);
 		});
-	}, [dispatch]);
+	}, [dispatch, setChecking, setIsLoggedIn]);
+
+	if (checking) {
+		return <h1>Espere...</h1>;
+	}
+
 	return (
 		<Router>
 			<div>
