6801d2899c9eab51c5728345f0713b02fc0169de
Luis Cusihuaman <luiscusihuaman88@gmail.com>
2020-07-27 15:49:52 -0300

    chore(notescreen): active only current note when change selection

--- src/components/notes/NoteScreen.js
+++ src/components/notes/NoteScreen.js
@@ -1,7 +1,19 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import { NotesAppBar } from "./NotesAppBar";
+import { useSelector } from "react-redux";
+import { useForm } from "../../hooks/useForm";
 
 export const NoteScreen = () => {
+	const { active: note } = useSelector((state) => state.notes);
+	const [formValues, handleInputChange, reset] = useForm(note);
+	const { body, title } = formValues;
+	const activeId = useRef(note.id);
+	useEffect(() => {
+		if (note.id !== activeId.current) {
+			reset(note);
+			activeId.current = note.id;
+		}
+	}, [note, reset]);
 	return (
 		<div className="notes__main-content">
 			<NotesAppBar />
@@ -11,17 +23,23 @@ export const NoteScreen = () => {
 					placeholder="Some awesome title"
 					className="notes__title-input"
 					autoComplete="off"
+					value={title}
+					onChange={handleInputChange}
 				/>
 				<textarea
 					placeholder="What happened today"
 					className="notes__textarea"
+					value={body}
+					onChange={handleInputChange}
 				></textarea>
-				<div className="notes__image">
-					<img
-						src="https://helpx.adobe.com/content/dam/help/en/stock/how-to/visual-reverse-image-search/jcr_content/main-pars/image/visual-reverse-image-search-v2_intro.jpg"
-						alt="imagen"
-					/>
-				</div>
+				{note.url && (
+					<div className="notes__image">
+						<img
+							src="https://helpx.adobe.com/content/dam/help/en/stock/how-to/visual-reverse-image-search/jcr_content/main-pars/image/visual-reverse-image-search-v2_intro.jpg"
+							alt="imagen"
+						/>
+					</div>
+				)}
 			</div>
 		</div>
 	);
--- src/hooks/useForm.js
+++ src/hooks/useForm.js
@@ -2,8 +2,8 @@ import { useState } from "react";
 
 export const useForm = (initialState = {}) => {
 	const [values, setValues] = useState(initialState);
-	const reset = () => {
-		setValues(initialState);
+	const reset = (newFormState = initialState) => {
+		setValues(newFormState);
 	};
 	const handleInputChange = ({ target }) => {
 		setValues({ ...values, [target.name]: target.value });
