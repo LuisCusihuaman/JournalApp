576fb870a8a46cc759688b9a529955d9aceb3802
Luis Cusihuaman <luiscusihuaman88@gmail.com>
2020-07-27 13:43:18 -0300

    chore: notesReducer

---
+++ src/reducers/notesReducer.js
@@ -0,0 +1,10 @@
+const initialState = {
+	notes: [],
+	active: null,
+};
+export const notesReducer = (state = initialState, action) => {
+	switch (action.type) {
+		default:
+			return state;
+	}
+};
