431b470a3fc2134b644a505f22fb845e3306fd76
Luis Cusihuaman <luiscusihuaman88@gmail.com>
2020-07-28 15:43:43 -0300

    test(notes): startLoadingNotes should load the notes

--- src/tests/actions/notes.test.js
+++ src/tests/actions/notes.test.js
@@ -1,16 +1,23 @@
 import configureStore from "redux-mock-store";
 import thunk from "redux-thunk";
-import { startNewNote } from "../../actions/notes";
+import { startNewNote, startLoadingNotes } from "../../actions/notes";
 import { db } from "../../firebase/firebase-config";
+import { types } from "../../types/types";
 
 const middlewares = [thunk]; // add your middlewares like `redux-thunk`
 const mockStore = configureStore(middlewares);
-const store = mockStore({
+const initState = {
 	auth: {
 		uid: "TESTING",
 	},
-});
+};
+
+let store = mockStore({ initState });
+
 describe("Pruebas con las actiosn de notes", () => {
+	beforeEach(() => {
+		store = mockStore(initState);
+	});
 	test("debe de crear una nueva nota startNewNote", async () => {
 		await store.dispatch(startNewNote());
 		const actions = store.getActions();
@@ -34,5 +41,20 @@ describe("Pruebas con las actiosn de notes", () => {
 		});
 		const docId = actions[0].payload.id;
 		await db.doc(`TESTING/journal/notes/${docId}`).delete();
+	});
+	test("startLoadingNotes debe cargar las notas", async () => {
+		await store.dispatch(startLoadingNotes("TESTING"));
+		const actions = store.getActions();
+		expect(actions[0]).toEqual({
+			type: types.notesLoad,
+			payload: expect.any(Array),
+		});
+		const expected = {
+			id: expect.any(String),
+			title: expect.any(String),
+			body: expect.any(String),
+			date: expect.any(Number),
+		};
+		expect(actions[0].payload[0]).toMatchObject(expected);
 	});
 });
