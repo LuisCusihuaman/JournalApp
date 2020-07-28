6cf74775d07253c14a95425c00c24eb37c124423
Luis Cusihuaman <luiscusihuaman88@gmail.com>
2020-07-28 02:36:33 -0300

    fix: text typo

--- src/components/journal/NothingSelected.js
+++ src/components/journal/NothingSelected.js
@@ -3,9 +3,12 @@ import React from "react";
 export const NothingSelected = () => {
 	return (
 		<div className="nothing__main-content">
-			<p>Select something</p>
-			pr create an entry!
-			<br />
+			<p>
+				Select something
+				<br />
+				pr create an entry!
+			</p>
+
 			<i className="far fa-star fa-4x mt-5"></i>
 		</div>
 	);
--- src/routers/PrivateRoute.js
+++ src/routers/PrivateRoute.js
@@ -6,7 +6,6 @@ export const PrivateRoute = ({
 	component: Component,
 	...rest
 }) => {
-	localStorage.setItem("lastPath", rest.location.pathname);
 	return (
 		<Route
 			{...rest}
