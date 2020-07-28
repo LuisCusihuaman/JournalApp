a93528815090d7f5228865b07ad4527555e43404
Luis Cusihuaman <luiscusihuaman88@gmail.com>
2020-07-28 20:25:53 -0300

    test: should be realize a startLogout

---
+++ src/tests/actions/auth.test.js
@@ -0,0 +1,49 @@
+import configureStore from "redux-mock-store";
+import thunk from "redux-thunk";
+const { login, logout, startLogout } = require("../../actions/auth");
+const { types } = require("../../types/types");
+
+const middlewares = [thunk]; // add your middlewares like `redux-thunk`
+const mockStore = configureStore(middlewares);
+const initState = {
+	auth: {
+		uid: "TESTING",
+	},
+	notes: {
+		active: {
+			id: "2s6vAD4ycd57Vwux8Gc8",
+			title: "Hola",
+			body: "Mundo",
+		},
+	},
+};
+
+let store = mockStore({ initState });
+describe("Pruebas con las acciones de Auth", () => {
+	beforeEach(() => {
+		store = mockStore(initState);
+	});
+	test("login y logout deben de crear la accion respectiva", () => {
+		const uid = "ABC123";
+		const displayName = "Luis";
+		const loginAction = login(uid, displayName);
+		const logoutAction = logout();
+		expect(loginAction).toEqual({
+            type: types.login, payload: {uid,displayName}
+        })
+		login();
+		expect(logoutAction).toEqual({ type: types.logout });
+	});
+	test("debe de realizar el startLogout", async () => {
+		await store.dispatch(startLogout());
+		const actions = store.getActions();
+		expect(actions[0]).toEqual({
+			type: types.logout,
+		});
+        expect(actions[1]).toEqual({type: types.notesLogoutCleaning})
+	});
+    test('debe de iniciar el start ', () => {
+        
+    })
+    
+});
