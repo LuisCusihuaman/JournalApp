62348c29a595191fa0040dcf7707c52bc13ea43f
Luis Cusihuaman <luiscusihuaman88@gmail.com>
2020-07-28 15:21:59 -0300

    chore(firebase): environment variables

--- src/firebase/firebase-config.js
+++ src/firebase/firebase-config.js
@@ -3,30 +3,15 @@ import "firebase/firestore";
 import "firebase/auth";
 
 const firebaseConfig = {
-	apiKey: "AIzaSyB68yahblj2W_JkzpFvMIk4Mi1BWU9ybnI",
-	authDomain: "courses-dev-a76cc.firebaseapp.com",
-	databaseURL: "https://courses-dev-a76cc.firebaseio.com",
-	projectId: "courses-dev-a76cc",
-	storageBucket: "courses-dev-a76cc.appspot.com",
-	messagingSenderId: "221692967572",
-	appId: "1:221692967572:web:d257166f13485badaa1281",
+	apiKey: process.env.REACT_APP_APIKEY,
+	authDomain: process.env.REACT_APP_AUTHDOMAIN,
+	databaseURL: process.env.REACT_APP_DATABASEURL,
+	projectId: process.env.REACT_APP_PROJECTID,
+	storageBucket: process.env.REACT_APP_STORAGEBUCKET,
+	messagingSenderId: process.env.REACT_APP_MESSAGINGSENDERID,
+	appId: process.env.REACT_APP_APPID,
 };
-const firebaseConfigTest = {
-	apiKey: "AIzaSyBGgn81NOtAI3MXHULz1tsHzaCu3J6uU24",
-	authDomain: "courses-test-f4643.firebaseapp.com",
-	databaseURL: "https://courses-test-f4643.firebaseio.com",
-	projectId: "courses-test-f4643",
-	storageBucket: "courses-test-f4643.appspot.com",
-	messagingSenderId: "222297997287",
-	appId: "1:222297997287:web:635d56c4e265da68a7c742",
-};
-// Initialize Firebase
-
-if (process.env.NODE_ENV === "test") {
-	firebase.initializeApp(firebaseConfigTest);
-} else {
-	firebase.initializeApp(firebaseConfig);
-}
+firebase.initializeApp(firebaseConfig);
 
 const db = firebase.firestore();
 const googleAuthProvider = new firebase.auth.GoogleAuthProvider();
